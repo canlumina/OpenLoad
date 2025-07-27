8f82dc2dd14a0bbc660a7d62190f75b4dff7591a
Ythlee <yc872027415@gmail.com>
2025-07-28 00:27:28 +0800

    测试stm32f1内部flash的分区功能

--- src/fal/inc/fal_cfg.h
+++ src/fal/inc/fal_cfg.h
@@ -14,18 +14,19 @@
 extern const struct fal_flash_dev stm32_onchip_flash;
 
 /* flash device table */
-#define FAL_FLASH_DEV_TABLE                                          \
-{                                                                    \
-    &stm32_onchip_flash,                                             \
-}
+#define FAL_FLASH_DEV_TABLE  \
+    {                        \
+        &stm32_onchip_flash, \
+    }
 
 /* ====================== Partition Configuration ========================== */
 #ifdef FAL_PART_HAS_TABLE_CFG
 /* partition table */
-#define FAL_PART_TABLE                                                                \
-{                                                                                     \
-    {FAL_PART_MAGIC_WORD, "app",        "stm32_onchip",                                         0, 959 * 1024, 0}, \
-}
+#define FAL_PART_TABLE                                                         \
+    {                                                                          \
+        {FAL_PART_MAGIC_WORD, "bootloader", "stm32_onchip", 0, 64 * 1024, 0},  \
+        {FAL_PART_MAGIC_WORD, "app"       , "stm32_onchip", 0, (512 - 64)* 1024, 0},        \
+    }
 #endif /* FAL_PART_HAS_TABLE_CFG */
 
 #endif /* _FAL_CFG_H_ */
