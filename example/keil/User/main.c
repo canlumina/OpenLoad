3761f80751846ebdea192582145a9ee287f3d68b
Ythlee <yc872027415@gmail.com>
2025-07-27 22:31:49 +0800

    重构fal，并在stm32f103上进行测试

--- example/keil/User/main.c
+++ example/keil/User/main.c
@@ -7,8 +7,12 @@
 #include "sys.h"
 #include "usart.h"
 #include "boot.h"
+#include <fal.h>
+#include <string.h>
 
 
+#define BUF_SIZE 1024
+static int fal_test(const char *partiton_name);
 
 int main(void)
 {
@@ -19,11 +23,157 @@ int main(void)
     led_init();                         /* 初始化LED */
     key_init();                         /* 初始化按键 */
     W25QXX_Init();                      // W25QXX初始化();
-
-    while(1)
+    fal_init();
+//	
+    if (fal_test("app") == 0)
     {
-
+        log_i("Fal partition (%s) test success!", "app");
+    }
+    else
+    {
+        log_e("Fal partition (%s) test failed!", "app");
     }
 
+//    if (fal_test("download") == 0)
+//    {
+//        log_i("Fal partition (%s) test success!", "download");
+//    }
+//    else
+//    {
+//        log_e("Fal partition (%s) test failed!", "download");
+//    }
+		
+		while(1)
+		{
+			delay_ms(500);
+		}
     return 0;
+}
+
+
+static int fal_test(const char *partiton_name)
+{
+    int ret;
+    int i, j, len;
+    uint8_t buf[BUF_SIZE];
+    const struct fal_flash_dev *flash_dev = NULL;
+    const struct fal_partition *partition = NULL;
+
+    if (!partiton_name)
+    {
+        log_e("Input param partition name is null!");
+        return -1;
+    }
+
+    partition = fal_partition_find(partiton_name);
+    if (partition == NULL)
+    {
+        log_e("Find partition (%s) failed!", partiton_name);
+        ret = -1;
+        return ret;
+    }
+
+    flash_dev = fal_flash_device_find(partition->flash_name);
+    if (flash_dev == NULL)
+    {
+        log_e("Find flash device (%s) failed!", partition->flash_name);
+        ret = -1;
+        return ret;
+    }
+
+    log_i("Flash device : %s   "
+          "Flash size : %dK   "
+          "Partition : %s   "
+          "Partition size: %dK", 
+           partition->flash_name, 
+           flash_dev->len/1024,
+           partition->name,
+           partition->len/1024);
+
+    /* 擦除 `partition` 分区上的全部数据 */
+    ret = fal_partition_erase_all(partition);
+    if (ret < 0)
+    {
+        log_e("Partition (%s) erase failed!", partition->name);
+        ret = -1;
+        return ret;
+    }
+    log_i("Erase (%s) partition finish!", partiton_name);
+
+    /* 循环读取整个分区的数据，并对内容进行检验 */
+    for (i = 0; i < partition->len;)
+    {
+        memset(buf, 0x00, BUF_SIZE);
+        len = (partition->len - i) > BUF_SIZE ? BUF_SIZE : (partition->len - i);
+
+        /* 从 Flash 读取 len 长度的数据到 buf 缓冲区 */
+        ret = fal_partition_read(partition, i, buf, len);
+        if (ret < 0)
+        {
+            log_e("Partition (%s) read failed!", partition->name);
+            ret = -1;
+            return ret;
+        }
+        for(j = 0; j < len; j++)
+        {
+            /* 校验数据内容是否为 0xFF */
+            if (buf[j] != 0xFF)
+            {
+                log_e("The erase operation did not really succeed!");
+                ret = -1;
+                return ret;
+            }
+        }
+        i += len;
+    }
+
+    /* 把 0 写入指定分区 */
+    for (i = 0; i < partition->len;)
+    {
+        /* 设置写入的数据 0x00 */
+        memset(buf, 0x00, BUF_SIZE);
+        len = (partition->len - i) > BUF_SIZE ? BUF_SIZE : (partition->len - i);
+
+        /* 写入数据 */
+        ret = fal_partition_write(partition, i, buf, len);
+        if (ret < 0)
+        {
+            log_e("Partition (%s) write failed!", partition->name);
+            ret = -1;
+            return ret;
+        }
+        i += len;
+    }
+    log_i("Write (%s) partition finish! Write size %d(%dK).", partiton_name, i, i / 1024);
+
+    /* 从指定的分区读取数据并校验数据 */
+    for (i = 0; i < partition->len;)
+    {
+        /* 清空读缓冲区，以 0xFF 填充 */
+        memset(buf, 0xFF, BUF_SIZE);
+        len = (partition->len - i) > BUF_SIZE ? BUF_SIZE : (partition->len - i);
+
+        /* 读取数据到 buf 缓冲区 */
+        ret = fal_partition_read(partition, i, buf, len);
+        if (ret < 0)
+        {
+            log_e("Partition (%s) read failed!", partition->name);
+            ret = -1;
+            return ret;
+        }
+        for(j = 0; j < len; j++)
+        {
+            /* 校验读取的数据是否为步骤 3 中写入的数据 0x00 */
+            if (buf[j] != 0x00)
+            {
+                log_e("The write operation did not really succeed!");
+                ret = -1;
+                return ret;
+            }
+        }
+        i += len;
+    }
+
+    ret = 0;
+    return ret;
 }
